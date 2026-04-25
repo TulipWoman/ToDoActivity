02cc11b1c0c5811bcd92410f07f620749ed0035f
TulipWoman <mia.g30@aol.com>
2026-04-25 23:29:52 +0100

    Week 6 half broke

--- app/src/main/java/com/example/todoactivity/TaskListActivity.java
+++ app/src/main/java/com/example/todoactivity/TaskListActivity.java
@@ -1,41 +1,74 @@
 package com.example.todoactivity;
 
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class TaskListActivity extends
     RecyclerView.Adapter<TaskListAdapter.RecyclerView.ViewHolder> {
 
+    private List<Task> tasks;
+    private TasksDB db;
+
+    public void setTaskList(TasksDB db, List<Task> tasks) {
+        this.db = db;
+        this.tasks = tasks;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
-    public TaskListAdapter.ViewHolder onCreateViewHolder(
-            @NonNull ViewGroup parent,
-            int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.task_layout,parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(
-            @NonNull TaskListAdapter.ViewHolder holder,
-            int position) {
-
+            @NonNull TaskListAdapter.ViewHolder holder, int position) {
+        final Task task = tasks.get(position);
+        holder.titleView.setText(task.title);
+        holder.descView.setText(task.description);
+        holder.imageView.setImageURI(Uri.parse(task.image));
     }
 
     @Override
     public int getItemCount(){
-        return 0;
+        if (tasks == null) return 0;
+        return tasks.size();
     }
+
+
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleView;
+        TextView descView;
+        ImageView imageView;
 
         public ViewHolder(@NonNull View itemView){
+
             super(itemView);
+
+            titleView = itemView.findViewById(R.id.taskListTitle);
+            descView = itemView.findViewById(R.id.taskListDesc);
+            imageView = itemView.findViewById(R.id.taskListImage);
+
         }
     }
-}
 
-private List<Task> tasks;
+
+    }
+
+
+
+
 
