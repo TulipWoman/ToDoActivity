37cfc65c4d8be0cbbdf41489496ce16941eec4f7
TulipWoman <mia.g30@aol.com>
2026-04-25 15:38:50 +0100

    Week 6 partial

---
+++ app/src/main/java/com/example/todoactivity/TaskListActivity.java
@@ -0,0 +1,41 @@
+package com.example.todoactivity;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskListActivity extends
+    RecyclerView.Adapter<TaskListAdapter.RecyclerView.ViewHolder> {
+
+    @NonNull
+    @Override
+    public TaskListAdapter.ViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull TaskListAdapter.ViewHolder holder,
+            int position) {
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
+    }
+}
+
+private List<Task> tasks;
+
